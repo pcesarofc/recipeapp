929092231e7e1b2c5680da19a832b1103b945f11
pcwos <p.cesarofc@gmail.com>
2021-07-13 20:10:59 -0300

    Category Screen criado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipeapp/components/category_item.dart';
+import 'screens/categories_screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,27 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Vamos cozinhar?'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
