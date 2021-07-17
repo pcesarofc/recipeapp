2415427f9687e0d48f11356e801e599e7ae6d88c
pcwos <p.cesarofc@gmail.com>
2021-07-16 21:49:01 -0300

    Adicionando detallhes ao item

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -12,7 +12,48 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: const Text('Receita'),
+      body: Column(
+        children: [
+          Container(
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+            width: double.infinity,
+            height: 300,
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              meal.title,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Text(
+                      meal.ingredients[index],
+                    ),
+                    color: Colors.amber,
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
