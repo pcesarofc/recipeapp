02932e33d82df1fc110a33dda44293eede21ca57
pcwos <p.cesarofc@gmail.com>
2021-07-19 19:44:00 -0300

    Finalizado

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -10,6 +10,8 @@ class CategoriesMealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
     final categoryMeals = DUMMY_MEALS.where((meals) {
